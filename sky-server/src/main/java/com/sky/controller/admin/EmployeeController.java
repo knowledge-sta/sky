f4db5dc3958ec00ac9a7f9d7837aeff90dd069a6
knowledgestar <3183878397@qq.coom>
2026-08-21 20:10:22 +0800

    编辑员工业务

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -97,4 +97,18 @@ public class EmployeeController {
         employeeService.startOrstop(status, id);
         return Result.success();
     }
+
+    @GetMapping("/{id}")
+    public Result<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("修改员工信息：{}", employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 }
