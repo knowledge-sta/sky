16d48eb4288b54f46a7be8090bb2b845884462b3
knowledgestar <3183878397@qq.coom>
2026-08-30 16:21:27 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishContronller.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishContronller.java
@@ -2,14 +2,15 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -23,6 +24,21 @@ public class DishContronller {
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分页查询：{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    //菜品删除
+    @DeleteMapping
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("删除菜品：{}", ids);
+        dishService.deleteBatch(ids);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,11 +1,21 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.data.geo.Distance;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface DishService {
 
+    void deleteBatch(List<Long> ids);
+
     public void saveWithFlavor(DishDTO dishDTO);
+
+    //菜品分页查询
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
