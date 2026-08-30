82fb86724b89d09849b3f7341d81def631597055
knowledgestar <3183878397@qq.coom>
2026-08-30 20:40:53 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 import org.springframework.data.geo.Distance;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +19,10 @@ public interface DishService {
     //菜品分页查询
 
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    //根据id查询菜品和对应的口味数据
+    DishVO getByIdWithFlavor(Long id);
+
+    //修改菜品
+    void updateWithFlavor(DishDTO dishDTO);
 }
