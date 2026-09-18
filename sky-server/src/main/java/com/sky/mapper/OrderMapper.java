b12ad878e88d3b9d6f1d0dde436554c2ed4cb62f
knowledgestar <3183878397@qq.coom>
2026-09-18 21:03:35 +0800

    营业额数据统计业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -41,4 +42,7 @@ public interface OrderMapper {
 
     @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
     List<Orders> getBystatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+
+    Double sumByMap(Map map);
 }
---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+
+import com.sky.vo.TurnoverReportVO;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public interface ReportService {
+
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+}
